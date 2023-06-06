24cf31b0f3e85276ee3528640831c20f924d8c5f
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2023-06-06 10:48:02 -0300

    feat: script nginx

---
+++ scripts/setup-nginx.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Atualização dos pacotes
+sudo apt update
+
+# Instalação do Nginx
+sudo apt install nginx -y
+
+# Criação de uma cópia do arquivo default
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default-original
+
+# Edição do arquivo default com o proxy pass usando o comando echo
+sudo echo "
+server {
+    listen 80;
+    server_name $MY_PUBLIC_DNS; 
+    location / {
+        proxy_pass http://localhost:8080/opencms/overview;
+    }
+}" >>  /etc/nginx/sites-available/default
+
+# Reinicialização do serviço do Nginx
+sudo service nginx restart
+
+# Verificação do status do Nginx
+sudo systemclt status nginx
